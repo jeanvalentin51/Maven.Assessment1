d5ce3ca4ebc54972a16c90400fd4d43599fa049e
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-28 13:19:43 -0400

    Add logic for delete odds

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -39,7 +39,27 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
-        return null;
+        Integer [] result;
+        int elementValue = 0;
+        int counter = 0;
+
+        for (int i = 0; i < ints.length; i++){
+            elementValue = ints[i];
+            if (elementValue%2!=0) counter++;
+        }
+
+        result = new Integer [ints.length - counter];
+        counter = 0;
+
+        for (int i = 0; i < ints.length; i++){
+            elementValue = ints[i];
+            if (elementValue%2==0) {
+                result[counter] = ints[i];
+                counter++;
+            }
+        }
+
+        return result;
     }
 
     /**
