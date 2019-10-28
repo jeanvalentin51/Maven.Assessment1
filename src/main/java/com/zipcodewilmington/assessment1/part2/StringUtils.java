bb0982a66571ba6e9f46c1839e404a7b21302af2
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-28 11:07:55 -0400

    Add logic for get first word

--- src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -23,7 +23,11 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
      */
     public static String getFirstWord(String sentence) {
-        return null;
+        String result = "";
+        String [] arrayOfWords = sentence.split(" ");
+
+        result = arrayOfWords[0];
+        return result;
     }
 
     /**
