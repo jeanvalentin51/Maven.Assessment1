5c7d2efb8a6ca0c5a5be4e0a78e7d3eea4e46a15
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-28 11:24:50 -0400

    Add logic for number of occurrences

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -11,7 +11,13 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+        int result = 0;
+
+        for (int i = 0; i < objectArray.length; i++){
+            if (objectArray[i].equals(objectToCount)) result++;
+        }
+
+        return result;
     }
 
     /**
