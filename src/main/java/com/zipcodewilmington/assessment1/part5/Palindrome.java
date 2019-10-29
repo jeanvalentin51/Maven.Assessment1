9a89edbfd934f355282a8dab3db3f730efbbda0c
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-29 13:00:46 -0400

    Finished logic for palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington.assessment1.part5;
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
 
 public class Palindrome {
 
@@ -8,14 +9,32 @@ public class Palindrome {
         stringLetters = input.split("");
 
         result = input.length();
-
+/*
         // get the doubles
         for (int i = 1; i < input.length() -1; i++) {
             if (i != input.length() - 1) {
                 if (stringLetters[i].equals(stringLetters[i + 1]) || stringLetters[i].equals(stringLetters[i - 1])) result++;
             }
         }
+*/
 
+        // added after
+
+        result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            // start from next letter
+            for (int x = i + 1; x < input.length()+1; x++) {
+                // check 0 1; 0 1 2; ....
+                // ... 1 2; 1 3;  ....
+                if (isPalindromic(input.substring(i,x))) {
+                    result++;
+                }
+            }
+        }
         return result;
+    }
+
+    public static boolean isPalindromic(String str) {
+        return str.equals(BasicStringUtils.reverse(str));
     }
 }
